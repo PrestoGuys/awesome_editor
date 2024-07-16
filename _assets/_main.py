d3ec8ffb92dde5f6b314187232be0d719bf32b31
PrestoGuys <prestoguys@gmail.com>
2024-07-16 17:46:41 -0500

    renamed main awe config file to main.toml, added to tkinter code

--- _assets/_main.py
+++ _assets/_main.py
@@ -2,5 +2,40 @@
 # Made by PrestoGuys in 2024
 # this is the main file of code for awesome editor
 
+import tkinter as tk
+from tkinter import ttk
+from tkinter import * 
+
 def main():
-	print("Hello, World!")
+	# opens the config file and saves it in a variable 
+	with open('CONFIG/main.toml', 'r') as f:
+		configtoml = toml.load(f)
+
+	# gets theme from config file
+	configtheme = configtoml['Settings']['Theme']
+
+	if configtheme == 'dark':
+		bgcolor = '#333333'
+
+	if configtheme == 'light':
+		bgcolor = '#fdfdfd'
+
+
+	if configtheme == 'dark':
+		fgcolor = '#fdfdfd'
+
+	if configtheme == 'light':
+		fgcolor = '#141414'
+
+
+
+	root = Tk()
+
+
+
+	# This is the section of code which creates the main window
+	root.geometry('640x480')
+	root.configure(background='#393939')
+	root.title('Awesome Editor - ')
+
+	root.mainloop()
