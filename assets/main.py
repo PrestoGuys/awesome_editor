fd92cb9d5ecfa12a4abf03b3ef1def7de9abbef8
PrestoGuys <prestoguys@gmail.com>
2024-07-20 14:44:45 -0500

    made unused.txt made syntax folder toml for mafkdown making main.py cleaner

--- assets/main.py
+++ assets/main.py
@@ -1,20 +1,27 @@
-# Awesome Editor v0.0.1 - July 16, 2024 
+# Awesome Editor v0.0.1 - July 16, 2024
 # Made by PrestoGuys in 2024
-# this is the main python file for awesome editor
-import toml
+# This is the main python file for The Awesome Editor.
 
+# Credit to www.codespeedy.com for some starter code.
+# Link: https://www.codespeedy.com/create-a-text-editor-in-python/
+
+import toml
+import json
+
+# Inporting Tkinter
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 from tkinter import filedialog
 
+
+# Links
 def main():
 	TextEditor()
-	
-# Defining TextEditor Class
+
+
 class TextEditor:
-	# Defining Constructor
 	def __init__(self, root):
 		# opens the config file and saves it in a variable 
 		with open('CONFIG/main.toml', 'r') as f:
@@ -48,19 +55,26 @@ class TextEditor:
 		print(fgcolor)
 
 
+
+		'''
+		# opens the config file and saves it in a variable 
+		with open('CONFIG/syntax_highlighting/markdown.toml', 'r') as f:
+			mdsyntax = toml.load(f)
+
+		print(mdsyntax["Main"]["file_ext"])
+		'''
+
+
+
+
 		# Assigning root
 		self.root = root
 		
-		#resolution = 720 # pixels, 4:3 aspect ratio
-
-		#tempres = resolution * 1.5 # 40 threes, just in case
-		#window_width  = round(tempres)
-		#window_height = resolution
-
+		# window width and window height of the program
 		window_width  = 1100
 		window_height = 750
 
-		#rint(tempres)
+
 		print(window_width)
 		print(window_height)
 
@@ -73,7 +87,7 @@ class TextEditor:
 
 		# gets senter of the screen
 		center_x = int(screen_width/2 - window_width / 2)
-		center_y = int((screen_height/2 - window_height / 2) + -50)
+		center_y = int(screen_height/2 - window_height / 2)
 
 		# sets window size and centers it
 		self.root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
@@ -96,26 +110,26 @@ class TextEditor:
 		self.status = StringVar()
 
 		# Creating Titlebar
-		self.titlebar = Label(self.root,textvariable=self.title,font=(configfont,15,"bold"),bd=2,relief=GROOVE)
+		self.titlebar = Label(self.root,textvariable=self.title,font=(configfont,15),bd=2,relief=GROOVE)
 		# Packing Titlebar to root window
 		self.titlebar.pack(side=TOP,fill=BOTH)
 		# Calling Settitle Function
 		self.settitle()
 
 		# Creating Statusbar
-		self.statusbar = Label(self.root,textvariable=self.status,font=(configfont,15,"bold"),bd=2,relief=GROOVE)
+		self.statusbar = Label(self.root,textvariable=self.status,font=(configfont,15),bd=2,relief=GROOVE)
 		# Packing status bar to root window
 		self.statusbar.pack(side=BOTTOM,fill=BOTH)
 		# Initializing Status
 		self.status.set("Welcome To Text Editor")
 
 		# Creating Menubar
-		self.menubar = Menu(self.root,font=(configfont,15,"bold"),activebackground="skyblue")
+		self.menubar = Menu(self.root,font=(configfont,15),activebackground="skyblue")
 		# Configuring menubar on root window
 		self.root.config(menu=self.menubar)
 
 		# Creating File Menu
-		self.filemenu = Menu(self.menubar,font=(configfont,12,"bold"),activebackground="skyblue",tearoff=0)
+		self.filemenu = Menu(self.menubar,font=(configfont,12),activebackground="skyblue",tearoff=0)
 		# Adding New file Command
 		self.filemenu.add_command(label="New",accelerator="Ctrl+N",command=self.newfile)
 		# Adding Open file Command
@@ -132,7 +146,7 @@ class TextEditor:
 		self.menubar.add_cascade(label="File", menu=self.filemenu)
 
 		# Creating Edit Menu
-		self.editmenu = Menu(self.menubar,font=(configfont,12,"bold"),activebackground="skyblue",tearoff=0)
+		self.editmenu = Menu(self.menubar,font=(configfont,12),activebackground="skyblue",tearoff=0)
 		# Adding Cut text Command
 		self.editmenu.add_command(label="Cut",accelerator="Ctrl+X",command=self.cut)
 		# Adding Copy text Command
@@ -147,14 +161,14 @@ class TextEditor:
 		self.menubar.add_cascade(label="Edit", menu=self.editmenu)
 
 		# Creating Help Menu
-		self.helpmenu = Menu(self.menubar,font=(configfont,12,"bold"),activebackground="skyblue",tearoff=0)
+		self.helpmenu = Menu(self.menubar,font=(configfont,12),activebackground="skyblue",tearoff=0)
 		# Adding About Command
 		self.helpmenu.add_command(label="About",command=self.infoabout)
 		# Cascading helpmenu to menubar
 		self.menubar.add_cascade(label="Help", menu=self.helpmenu)
 
 		# Creating Help Menu
-		self.ggmenu = Menu(self.menubar,font=(configfont,12,"bold"),activebackground="skyblue",tearoff=0)
+		self.ggmenu = Menu(self.menubar,font=(configfont,12),activebackground="skyblue",tearoff=0)
 		# Adding About Command
 		self.ggmenu.add_command(label="ghgh",command=self.bruhaddd)
 		# Cascading helpmenu to menubar
@@ -163,7 +177,7 @@ class TextEditor:
 		# Creating Scrollbar
 		scrol_y = Scrollbar(self.root,orient=VERTICAL)
 		# Creating Text Area
-		self.txtarea = Text(self.root,yscrollcommand=scrol_y.set,font=(configfont,15,"bold"),state="normal",relief=GROOVE)
+		self.txtarea = Text(self.root,yscrollcommand=scrol_y.set,font=(configfont,15),state="normal",relief=GROOVE)
 		# Packing scrollbar to root window
 		scrol_y.pack(side=RIGHT,fill=Y)
 		# Adding Scrollbar to text area
@@ -287,7 +301,7 @@ class TextEditor:
 
 	# Defining Copy Funtion
 	def copy(self,*args):
-					self.txtarea.event_generate("<<Copy>>")
+		self.txtarea.event_generate("<<Copy>>")
 
 	# Defining Paste Funtion
 	def paste(self,*args):
@@ -343,14 +357,12 @@ class TextEditor:
 		# Binding Ctrl+a to saveasfile funtion
 		self.txtarea.bind("<Control-a>",self.saveasfile)
 		# Binding Ctrl+e to exit funtion
-		self.txtarea.bind("<Control-e>",self.exit)
-		# Binding Ctrl+x to cut funtion
+		self.txtarea.bind("<Control-q>",self.exit)
+
 		self.txtarea.bind("<Control-x>",self.cut)
-		# Binding Ctrl+c to copy funtion
 		self.txtarea.bind("<Control-c>",self.copy)
-		# Binding Ctrl+v to paste funtion
 		self.txtarea.bind("<Control-v>",self.paste)
-		# Binding Ctrl+u to undo funtion
+
 		self.txtarea.bind("<Control-u>",self.undo)
 
 # Creating TK Container
